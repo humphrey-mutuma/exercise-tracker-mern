71a9c19f7e9bea92cb6248fe8329bb22ab547a79
humphrey-mutuma <humphreymutuma01@gmail.com>
2022-01-06 19:27:10 +0300

    backend complete

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,14 @@ import express from "express";
 import User from "../models/user.model.js";
 const router = express.Router();
 
+// fetch all users
 router.route("/").get((req, res) => {
   User.find()
     .then((users) => res.json(users))
     .catch((err) => res.status(400).json("Error" + err));
 });
 
+// add a user
 router.route("/add").post((req, res) => {
   const username = req.body.username;
 
